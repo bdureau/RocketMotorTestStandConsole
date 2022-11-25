a13d0c68ddbe511ae6e1135788f6db22aaeb2850
bdureau <boris.dureau@neuf.fr>
2022-11-25 13:44:38 +0100

    latest build nbr

--- app/build/generated/source/buildConfig/debug/com/rocketmotorteststand/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/rocketmotorteststand/BuildConfig.java
@@ -8,5 +8,5 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.rocketmotorteststand";
   public static final String BUILD_TYPE = "debug";
   public static final int VERSION_CODE = 10;
-  public static final String VERSION_NAME = "2";
+  public static final String VERSION_NAME = "2.0.0";
 }
